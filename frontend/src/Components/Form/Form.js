d0a05fd173893423db6fbb822eb291e5225c89a8
jaya surya <suryajaya4899@gmail.com>
2024-08-30 14:25:58 +0530

    url updated

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -13,7 +13,7 @@ const Form = () => {
     const [mobileField, setMobileField] = useState('');
     const [error, setError] = useState("");
 
-    const baseUrl = 'http://localhost:3001';
+    const baseUrl = 'https://new-portfolio-8gzn.onrender.com';
 
     const handleSubmit = async (event) => {
         event.preventDefault();
