8b932197c38c7c1898ccab72395c1cbf143bd634
jaya surya <suryajaya4899@gmail.com>
2024-11-12 15:32:03 +0530

    seo helmet package added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './Responsive.css';
+import { Helmet } from "react-helmet";
 
 import './assets/Fonts/Fonts.css';
 import NavBar from './Components/NavBar/NavBar';
@@ -9,6 +10,20 @@ import resume from './assets/resume.pdf';
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Jaya Surya",
+              "jobTitle": "Front-end Developer",
+              "url": "https://jayasurya-portfolio-new.vercel.app/",
+              "description": "Front-end Developer specializing in React and UI design."
+            }
+          `}
+        </script>
+      </Helmet>
       <NavBar />
       <Button isDownload={true} link={resume}>Download Resume</Button>
     </div>
