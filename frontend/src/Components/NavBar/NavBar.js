1048bac6e7b9044e2d957f1493cc12fd78774aa9
jaya surya <suryajaya4899@gmail.com>
2025-01-17 15:59:06 +0530

    mobile menu fix

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -9,6 +9,7 @@ import About from '../About/About';
 const NavBar = () => {
     const [isSticky, setIsSticky] = useState(false);
     const [isMobileMenu, setIsMobileMenu] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);  // Track window width
     const projectsRef = useRef(null);
     const pageRef = useRef(null);
     const footerRef = useRef(null);
@@ -28,26 +29,52 @@ const NavBar = () => {
         };
     }, []);
 
+    // Update the window width on resize
     useEffect(() => {
-        const updateFooterWrapHwight = () => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        const updateFooterWrapHeight = () => {
             if (navbarRef.current) {
                 setHeaderHeight(navbarRef.current.offsetHeight);
             }
-        }
-        updateFooterWrapHwight();
-        window.addEventListener('resize', updateFooterWrapHwight);
+        };
+        updateFooterWrapHeight();
+        window.addEventListener('resize', updateFooterWrapHeight);
 
         return () => {
-            window.addEventListener('resize', updateFooterWrapHwight);
+            window.removeEventListener('resize', updateFooterWrapHeight);
         };
     }, [navbarRef]);
+
+    // Update body class when isMobileMenu changes
+    useEffect(() => {
+        if (isMobileMenu) {
+            document.body.classList.add('open');
+        } else {
+            document.body.classList.remove('open');
+        }
+
+        // Cleanup to remove class on component unmount
+        return () => {
+            document.body.classList.remove('open');
+        };
+    }, [isMobileMenu]);
 
     const sectionScroll = (ref, isHome = false) => {
         const navbar = document.querySelector('.navbar .nav_wrap');
         const navbarHeight = navbar.offsetHeight;
 
         const top = isHome ? 0 : ref.current.getBoundingClientRect().top + window.scrollY - navbarHeight;
-        console.log(top);
         window.scrollTo({ top, behavior: 'smooth' });
     };
 
@@ -61,41 +88,90 @@ const NavBar = () => {
                 <Center noPaddingY>
                     <div className="nav_wrap Comfortaa">
                         <div className="logo_text" onClick={() => {
-                            sectionScroll(pageRef, true)
-                            setIsMobileMenu(false)
-                        }}>Jaya Surya</div>
-                        <ul className={`menu_items ${isMobileMenu ? 'active' : ''}`}>
-                            <li className="menu_item">
-                                <div onClick={() => {
-                                    sectionScroll(aboutRef)
-                                    setIsMobileMenu(false)
-                                }}>About</div>
-                            </li>
-                            <li className="menu_item">
-                                <div onClick={() => {
-                                    sectionScroll(projectsRef)
-                                    setIsMobileMenu(false)
-                                }}>Projects</div>
-                            </li>
-                            <li className="menu_item">
-                                <div onClick={() => {
-                                    sectionScroll(footerRef)
-                                    setIsMobileMenu(false)
-                                }}>Contact</div>
-                            </li>
-                        </ul>
+                            sectionScroll(pageRef, true);
+                            setIsMobileMenu(false);
+                        }}>
+                            Jaya Surya
+                        </div>
+                        {windowWidth > 900 ? (
+                            <ul className="menu_items">
+                                <li className="menu_item">
+                                    <div onClick={() => {
+                                        sectionScroll(aboutRef);
+                                        setIsMobileMenu(false);
+                                    }}>About</div>
+                                </li>
+                                <li className="menu_item">
+                                    <div onClick={() => {
+                                        sectionScroll(projectsRef);
+                                        setIsMobileMenu(false);
+                                    }}>Projects</div>
+                                </li>
+                                <li className="menu_item">
+                                    <div onClick={() => {
+                                        sectionScroll(footerRef);
+                                        setIsMobileMenu(false);
+                                    }}>Contact</div>
+                                </li>
+                            </ul>
+                        ) : (
+                            <div className={`menu_items ${isMobileMenu ? 'active' : ''}`}>
+                                <ul className="menu_items_wrap">
+                                    <li className="menu_item">
+                                        <div onClick={() => {
+                                            sectionScroll(aboutRef);
+                                            setIsMobileMenu(false);
+                                        }}>About</div>
+                                    </li>
+                                    <li className="menu_item">
+                                        <div onClick={() => {
+                                            sectionScroll(projectsRef);
+                                            setIsMobileMenu(false);
+                                        }}>Projects</div>
+                                    </li>
+                                    <li className="menu_item">
+                                        <div onClick={() => {
+                                            sectionScroll(footerRef);
+                                            setIsMobileMenu(false);
+                                        }}>Contact</div>
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
                         <div className={`mobile_menu ${isMobileMenu ? 'active' : ''}`} onClick={openMenu}>
                             <span className="line"></span>
                         </div>
                     </div>
-                </Center >
+                </Center>
             </div>
-            <Hero headerHeight={headerHeight} ref={pageRef} jobTitle="Front-end Developer" name='Jaya Surya' />
+            <Hero headerHeight={headerHeight} ref={pageRef} jobTitle="Front-end Developer" name="Jaya Surya" />
             <About ref={aboutRef} />
             <Projects ref={projectsRef} />
             <Footer ref={footerRef} />
+            <div className="mobile_menu_wrapper">
+                <ul className="mobile_menu_items active">
+                    <li className="menu_item">
+                        <div onClick={() => {
+                            sectionScroll(aboutRef);
+                            setIsMobileMenu(false);
+                        }}>About</div>
+                    </li>
+                    <li className="menu_item">
+                        <div onClick={() => {
+                            sectionScroll(projectsRef);
+                            setIsMobileMenu(false);
+                        }}>Projects</div>
+                    </li>
+                    <li className="menu_item">
+                        <div onClick={() => {
+                            sectionScroll(footerRef);
+                            setIsMobileMenu(false);
+                        }}>Contact</div>
+                    </li>
+                </ul>
+            </div>
         </>
     );
-}
+};
 
 export default NavBar;
